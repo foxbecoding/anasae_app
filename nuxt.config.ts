774f611553dbb97cb2a243bf4cf540912ced4ded
FoxBeCoding <fox@foxbecoding.com>
2023-07-21 14:28:00 -0500

    updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,24 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
         BASE_URL: process.env.BASE_URL,
-        
+        CDN_URL: process.env.CDN_URL,
+        API_BASE_URL: process.env.API_BASE_URL
     }
   },
-  app: {},
+  app: {
+    head: { 
+      htmlAttrs: {
+          lang: 'en'
+      },
+      meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' }, 
+      ],
+      link: [
+          { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]   
+    }
+  },
   css: [
       'vuetify/lib/styles/main.sass', 
       '@mdi/font/css/materialdesignicons.min.css',
