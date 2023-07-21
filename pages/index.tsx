0a649b5aad0c8bc5cad6c2b034e32c6674f9073a
FoxBeCoding <fox@foxbecoding.com>
2023-07-21 17:33:38 -0500

    updated packages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
 export default defineComponent({
-    render (lang="ts") {
+    render () {
         const test = ref<boolean>(true)
-        const list = ref<string>([
+        const list = ref<{id: number, name: string}[]>([
             { id: 1, name: 'Desmond' },
             { id: 2, name: 'Lavell' },
             { id: 3, name: 'Fox' }
         ])
+        // const gg = ref<any>(
+        //     <v-btn color="surface" flat={true}>Fox</v-btn>
+        // )
         return ( 
             <div>
                 <h1>FoxBeCoding</h1>
-                { test.value ? <v-btn color="surface" flat="true">Fox</v-btn> : null }
+                { test.value ? <v-btn color="surface" flat={true}>Fox</v-btn> : null }
+                <ul>
+                    {list.value.map(({ id, name }) => {
+                        return <li key={id}>{id+' '+name}</li>
+                    })}
+                </ul>
             </div>
         )
     }
