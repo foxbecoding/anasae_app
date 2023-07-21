fd86510f831f51a539a6a6009216e164c1a94576
FoxBeCoding <fox@foxbecoding.com>
2023-07-21 14:52:10 -0500

    updated vuetify

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,31 +1,44 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import { VVirtualScroll } from 'vuetify/labs/VVirtualScroll'
 
 export default defineNuxtPlugin(nuxtApp => {
-  const darkTheme = {
-    dark: true,
-    colors: {
-      background: '#121212',
-      surface: '#212121',
-      primary: '#3fabf3',
-      error: '#EF5350',
-      info: '#2196F3',
-      success: '#4CAF50',
-      warning: '#FB8C00',
-    }
-  } 
-  const vuetify = createVuetify({
-    ssr: true,
-    components: { ...components, VVirtualScroll },
-    directives,
-    theme: {
-      defaultTheme: 'darkTheme',
-      themes: {
-        darkTheme,
-      }
-    }
-  })
-  nuxtApp.vueApp.use(vuetify)
+    const anasaeDark = {
+        dark: true,
+        colors: {
+            background: '#121212',
+            surface: '#212121',
+            'surface-alt': '#f1f1f1',
+            primary: '#3fabf3',
+            error: '#EF5350',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FB8C00',
+        }
+    } 
+    const anasaeLight = {
+        dark: false,
+        colors: {
+            background: '#ffffff',
+            surface: '#efefef',
+            'surface-alt': '#0f0f0f',
+            primary: '#3fabf3',
+            error: '#EF5350',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FB8C00',
+        }
+    } 
+    const vuetify = createVuetify({
+        ssr: true,
+        components: { ...components },
+        directives,
+        theme: {
+            defaultTheme: 'anasaeDark',
+            themes: {
+                anasaeDark, anasaeLight
+            }
+        }
+    })
+    nuxtApp.vueApp.use(vuetify)
 })
