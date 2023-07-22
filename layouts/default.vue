cb93e0a56a19ae43863902e9ea1b266588488bcc
FoxBeCoding <fox@foxbecoding.com>
2023-07-21 21:28:54 -0500

    updated layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,9 @@ const toggleTheme = (): void => {
 <template>
     <v-app>
         <!-- <v-container></v-container> -->
-        <v-app-bar></v-app-bar>
+        <v-app-bar>
+            <v-container class="px-4" fluid>LOGO</v-container>
+        </v-app-bar>
         <v-main>
             <v-card>
                 <v-card-actions></v-card-actions>
