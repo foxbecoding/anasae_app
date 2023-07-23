50e902faa76e53f1bb36e8e5369ed0ecd7dced5c
FoxBeCoding <fox@foxbecoding.com>
2023-07-23 02:27:40 -0500

    updated components, pages

--- components/AppBar/Start.vue
+++ components/AppBar/Start.vue
@@ -3,13 +3,12 @@ import { useTheme } from 'vuetify'
 
 const config = useRuntimeConfig()
 const theme = useTheme()
-const LOGO = computed<string>((): string => theme.global.current.value.dark ? 'logo-dark' : 'logo-light')
+const LOGO = computed<string>((): string => theme.global.current.value.dark ? 'logo-dark-v1' : 'logo-light-v1')
 
 </script>
 
 <template>
     <div id="start" class="d-flex flex-row align-center">
-        <v-img :src="`${config.public.CDN_MEDIA_URL}/${LOGO}.png`" width="120" />
-        <v-app-bar-title v-if="false" class="nana-logo-title">ANASAE</v-app-bar-title>
+        <v-img :src="`${config.public.CDN_MEDIA_URL}/assets/${LOGO}.png`" width="120" />
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-const test = useRuntimeConfig()
-const dsd = computed(() => {})
 </script>
 
 <template>
-    {{ test.public.CDN_MEDIA_URL }}
+   
 </template>
