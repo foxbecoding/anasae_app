c6e407849068c590c359efb543360fa3ec3e105c
FoxBeCoding <fox@foxbecoding.com>
2023-07-30 15:31:52 -0500

    updated test

---
+++ tests/composables/mediaAssets.nuxt.spec.ts
@@ -0,0 +1,7 @@
+import { useMediaAssets } from '@/composables/mediaAssets'
+
+describe('useMediaAssets composable', () => {
+    test('composable', () => {
+        expect(useMediaAssets).toBeTruthy()
+    })
+})
