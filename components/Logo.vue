1ce732f9d09598ced58583c0bc2d911fdce9735d
FoxBeCoding <fox@foxbecoding.com>
2023-08-01 12:37:41 -0500

    updated components

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useTheme } from 'vuetify'
+
 const dirPath = ref<string>('/assets/')
 const config = useRuntimeConfig()
 const theme = useTheme()
@@ -7,6 +8,7 @@ const Logo = computed<string>((): string => {
     const theme_color = theme.global.current.value.dark ? `${dirPath.value}logo-dark-v1.png` : `${dirPath.value}logo-light-v1.png`
     return `${config.public.CDN_MEDIA_URL}${theme_color}`
 })
+
 </script>
 
 <template>
