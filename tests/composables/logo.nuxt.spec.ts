70fa4493f3d2a0d5c13e8b7b4413ff4a5d236c60
FoxBeCoding <fox@foxbecoding.com>
2023-08-01 11:52:15 -0500

    updated test

---
+++ tests/composables/logo.nuxt.spec.ts
@@ -0,0 +1,16 @@
+import { mockComponent, mountSuspended, mockNuxtImport } from 'nuxt-vitest/utils'
+import  { useLogo } from '@/composables/logo'
+
+describe('useLogo composable', async () => {
+    afterEach(() => {
+        vi.unstubAllGlobals()
+    })
+    test('composable', async () => {
+        // const cdnPath = 'http://192.168.1.235:3058'
+        // const assetPath = '/media/slider/slide-TBbkyQm0I-1690339280.png'
+        // const res = await useMediaAssets(assetPath)
+        // const asset = res.Asset.value
+        // expect(asset).toBe(cdnPath+assetPath)
+        expect(useLogo).toBeTruthy()
+    })
+})
