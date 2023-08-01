f5acd52379b38624c5fd421ad6d2d6af59b97cee
FoxBeCoding <fox@foxbecoding.com>
2023-08-01 12:35:06 -0500

    updated components

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { useTheme } from 'vuetify'
-const dir_path = ref<string>('/assets/')
+const dirPath = ref<string>('/assets/')
 const config = useRuntimeConfig()
 const theme = useTheme()
 const Logo = computed<string>((): string => {
-    const theme_color = theme.global.current.value.dark ? `${dir_path.value}logo-dark-v1.png` : `${dir_path.value}logo-light-v1.png`
+    const theme_color = theme.global.current.value.dark ? `${dirPath.value}logo-dark-v1.png` : `${dirPath.value}logo-light-v1.png`
     return `${config.public.CDN_MEDIA_URL}${theme_color}`
 })
 </script>
 
 <template>
-    <NuxtLink :to="{name: 'clipz'}" class="w-100">
+    <NuxtLink :to="{name: 'index'}" class="w-100">
         <v-img 
             id="logo" 
             data-test-id="logo"
