0503549a9dfe117edd3ad28bf107f20fff9b131b
FoxBeCoding <fox@foxbecoding.com>
2023-08-05 16:46:18 -0500

    updated components

---
+++ components/Auth/LoginForm.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const dialog = ref<boolean>(true)
+</script>
+
+
+<template>
+    <v-dialog
+      v-model="dialog"
+      persistent
+      width="500px"
+    >
+        <v-card height="1000px" rounded="xl">
+            <v-container>
+                <div class="d-flex justify-space-between align-center">
+                    <v-card-title class="text-h4">
+                    Login
+                    </v-card-title>
+                    <div class="auth-form-logo-container w-100 ">
+                        <Logo />
+                    </div>
+                </div>
+                <v-card-text>Login form </v-card-text>
+            </v-container>
+        </v-card>
+    </v-dialog>
+</template>
--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,27 +1,9 @@
 <script setup lang="ts">
 import Home from '@/pages/index.vue'
-const dialog = ref<boolean>(true)
-
 </script>
 
 
 <template>
-    <v-dialog
-      v-model="dialog"
-      persistent
-      width="600px"
-    >
-        <v-card height="1000px" rounded="xl">
-            <v-container>
-                <div class="d-flex justify-space-between">
-                    <v-card-title class="text-h4">
-                    Login
-                    </v-card-title>
-                    <Logo />
-                </div>
-                <v-card-text>Login form </v-card-text>
-            </v-container>
-        </v-card>
-    </v-dialog>
+    <AuthLoginForm />
     <Home />
 </template>
