d72a1e210d55fb424b12eb2731302d931ef89755
FoxBeCoding <fox@foxbecoding.com>
2023-08-05 20:41:05 -0500

    updated composables

--- composables/auth.ts
+++ composables/auth.ts
@@ -4,10 +4,11 @@ import ForgotForm from '@/components/Auth/ForgotForm.vue'
 import { RouteName } from '@/utils/types'
 
 type ComponentFormType = typeof LoginForm | typeof SignUpForm | typeof ForgotForm
+type FormTitleType = 'Login' | 'Sign Up' | 'Forgot Password'
 
 export const useAuthFormFactory = () => {
     const route = useRoute()
-    const routeNames = ref<RouteName[]>(['auth-login', 'auth-sign-up', 'auth-forgot'])
+    const routeNames = shallowRef<RouteName[]>(['auth-login', 'auth-sign-up', 'auth-forgot'])
     const components = shallowRef({
         "auth-login": LoginForm, 
         "auth-sign-up": SignUpForm, 
@@ -15,5 +16,23 @@ export const useAuthFormFactory = () => {
     })
 
     const ComponentForm = computed((): ComponentFormType => components.value[route.name as RouteName] )
-    return { ComponentForm, routeNames }
+    const FormTitle = computed((): FormTitleType => {
+        let title = '' as FormTitleType
+        switch (route.name as RouteName) {
+            case 'auth-login':
+                title = 'Login'
+                break;
+            case 'auth-sign-up':
+                title = 'Sign Up'
+                break;
+            case 'auth-forgot':
+                title = 'Forgot Password'
+                break;
+            default:
+                break;
+        }
+        return title
+    })
+
+    return { ComponentForm, FormTitle, routeNames }
 }
