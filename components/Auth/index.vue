df68ade1c1ab8381223d000726fe0910b7f27f14
FoxBeCoding <fox@foxbecoding.com>
2023-08-07 12:04:02 -0500

    updated components

--- components/Auth/index.vue
+++ components/Auth/index.vue
@@ -5,6 +5,13 @@ const props = defineProps({
 })
 
 const dialog = ref<boolean>(true)
+
+const close = async (): Promise<void> => { 
+    dialog.value = false
+    setTimeout(() => {
+        useRouter().back()
+    }, 500) 
+}
 </script>
 
 <template>
@@ -14,11 +21,19 @@ const dialog = ref<boolean>(true)
         width="500px"
     >
         <v-card height="1000px" rounded="xl">
-            <v-container>
+            <v-container fluid>
                 <div class="d-flex justify-space-between align-center">
-                    <v-card-title class="text-h4"> {{props.title}} </v-card-title>
-                    <div class="auth-form-logo-container w-100 "> <Logo /> </div>
+                    <v-btn @click="close" size="small" icon>
+                        <v-icon>mdi-close</v-icon>
+                    </v-btn> 
+                    
+                    <div class="auth-form-logo-container w-100 mr-4"> 
+                        <Logo @click.stop.prevent="false"/> 
+                    </div>
                 </div>
+                <v-card-title class="text-h5">
+                    {{props.title}} 
+                </v-card-title>
                 <v-card-text> <slot /> </v-card-text>
             </v-container>
         </v-card>
