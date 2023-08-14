a541223b2faafe08dc548211880ec4f5cd37af69
FoxBeCoding <fox@foxbecoding.com>
2023-08-13 20:13:12 -0500

    updated app

--- app.vue
+++ app.vue
@@ -9,14 +9,21 @@ const userStore = useUserStore()
 const initialize = async (): Promise<void> => {
   userStore.init()
   sliderStore.init()
+  nextTick(async () => {
+    await useApi({
+      method: 'GET',
+      path: `${config.public.API_X_FCT}`
+    })
+  })
+  
 }
 
 initialize()
 
 onMounted(async () => {
-  nextTick(() => {
-    useFetch(`${config.public.API_BASE_URL}${config.public.API_X_FCT}`)
-  })
+  // nextTick(() => {
+  //   useFetch(`${config.public.API_BASE_URL}${config.public.API_X_FCT}`)
+  // })
 })
 </script>
 
