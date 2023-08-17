a22b514f16bfa25b8abb204debcb00a0617a0c79
FoxBeCoding <fox@foxbecoding.com>
2023-08-16 23:06:20 -0500

    updated assets, pages

--- pages/profile/[uid].vue
+++ pages/profile/[uid].vue
@@ -4,36 +4,31 @@ import { useAuthStore } from '@/store'
 
 const config = useRuntimeConfig()
 const route = useRoute()
-const profile = ref<User>()
+const profile = ref<User>({} as User)
 const isOwner = ref<boolean>()
 
 const getProfile = async (): Promise<void> => {
     const { data: cacheData } = useNuxtData(`${route.params.uid}`)
     profile.value = cacheData?.value?.user
     isOwner.value = cacheData?.value?.owner
-
+    
     const { data, pending, error, refresh } = await useApi({
         path: `${config.public.API_USER_PROFILE}${route.params.uid}/`,
         method: 'GET',
         key: `${route.params.uid}`
     })
+
     profile.value = data.value.user
     isOwner.value = data.value.owner
-    
 }
+
 getProfile()
 
 </script>
 
 <template>
     <v-container class="layout-px" fluid>
-        <!-- <div v-if="userProfile">{{userProfile}}</div> -->
-        <h1 v-show="profile">{{profile.username}}</h1>
-        <div>{{ isOwner }}</div>
-        <!-- <v-skeleton-loader v-else
-          color="background"
-          max-width="500"
-          type="avatar, list-item, text@3" 
-        /> -->
+        <v-img v-if="profile.image" :src="profile.image" />
+        <v-icon v-else size="150" class="mask mask-squircle">mdi-account-circle</v-icon>
     </v-container>
 </template>
